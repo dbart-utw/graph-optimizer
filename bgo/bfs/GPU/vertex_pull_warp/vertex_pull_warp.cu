7a1dad1b78ca7a39a8ded6fdf7862d3690dfd300
Duncan Bart <duncanbart01@gmail.com>
2025-11-12 15:52:33 +0100

    Added missing bfs GPU implementations

--- bgo/bfs/GPU/vertex_pull_warp/vertex_pull_warp.cu
+++ bgo/bfs/GPU/vertex_pull_warp/vertex_pull_warp.cu
@@ -1,1 +1,8 @@
-#include "bfs.hpp"
+#include "vertex_pull_warp.hpp"
+
+int bfs_vertex_pull(CSR &g, CArray<int32_t> *levels) {
+    levels->init(g.num_nodes());
+    BFSGPU<Reduction<normal>>(g, levels->data, VERTEX_PULL_WARP);
+    
+    return 0;
+}
--- bgo/bfs/GPU/vertex_push_warp/vertex_push_warp.cu
+++ bgo/bfs/GPU/vertex_push_warp/vertex_push_warp.cu
@@ -1,1 +1,8 @@
-#include "bfs.hpp"
+#include "vertex_push_warp.hpp"
+
+int bfs_vertex_push_warp(CSR &g, CArray<int32_t> *levels) {
+    levels->init(g.num_nodes());
+    BFSGPU<Reduction<normal>>(g, levels->data, VERTEX_PUSH_WARP);
+    
+    return 0;
+}
